bf2e8ecc56f9699e684bf11720da64c869078301
zionakwubo01 <zionakwubo@gmail.com>
2023-12-06 18:16:37 +0100

    fixing

--- src/Authflow/Register.tsx
+++ src/Authflow/Register.tsx
@@ -6,7 +6,8 @@ const Register = () => {
 
     const schema = yup.object({
         userName: yup.string().required("enter your user name"),
-        password: yup.string().required("entervyour password")
+        password: yup.string().required("entervyour password"),
+        email: yup.string().email().required("enter your email")
     })
 
 
@@ -14,15 +15,17 @@ const Register = () => {
         reset,
         register,
         handleSubmit,
+        formState: { errors }
     } = useForm({
         resolver: yupResolver(schema)
     })
 
 
     const handlesubmitshow = handleSubmit((data: any) => {
-        const { userName, password } = data
-        createUser({ userName, password }).then((res: any) => {
-            localStorage.setItem("user", JSON.stringify(res.data))
+        const { userName, password, email } = data
+        createUser({ userName, password, email }).then((res: any) => {
+
+            console.log(res)
         })
     })
 
@@ -31,51 +34,67 @@ const Register = () => {
             <div className="w-[100%] h-[100vh] 
             flex items-center justify-center
             ">
-                <div className="w-[370px] min-h 
+                <form onSubmit={handlesubmitshow}>
+                    <div className="w-[370px] min-h 
                  bg-white flex flex-col items-center justify-center
                  gap-[20px] border-solid border-[0.5px] border-gray-400
                  rounded-[5px]
                  ">
-                    <div className="font-bold text-[40px]
+                        <div className="font-bold text-[40px]
                     mt-[50px] 
                     ">Sign up</div>
-                    <div className="w-[96%] min-h 
+                        <div className="w-[96%] min-h 
                     flex flex-col gap-[15px]
                     ">
-                        <input
-                            placeholder="enter your email address"
-                            className="h-[40px] w-[100%] bg-white
+                            <input
+                                type="text"
+                                placeholder="enter your name"
+                                className="h-[40px] w-[100%] bg-white
                      rounded-[5px] border-[1px] border-solid border-blue-700
-placeholder:pl-[10px]
-                    
+placeholder:pl-[10px] 
                      "
-                        />
-                        <input
-                            placeholder="enter password"
-                            className="h-[40px] w-[100%] bg-white
+                                {...register("userName")}
+                            />
+                            <input
+                                placeholder="enter password"
+                                className="h-[40px] w-[100%] bg-white
                      rounded-[5px]  border-[1px] border-solid border-blue-700
 placeholder:pl-[10px]
                      "
-                        />
-                    </div>
-                    <button
-                        className="w-[350px] h-[40px] bg-white
+                                type="password"
+                                {...register("password")}
+                            />
+                            <input
+                                placeholder="enter email"
+                                className="h-[40px] w-[100%] bg-white
+                     rounded-[5px]  border-[1px] border-solid border-blue-700
+placeholder:pl-[10px]
+                     "
+                                type="email"
+                                {...register("email")}
+                            />
+                        </div>
+                        <button
+                            className="w-[350px] h-[40px] bg-white
                         border-[1px] border-solid
                         border-gray-500 rounded-[5px]
                         "
-                    >
-                        continue with google
-                    </button>
-                    <button
-                        className="w-[350px] h-[40px] bg-black
+                        >
+                            continue with google
+                        </button>
+                        <button
+                            type="submit"
+                            className="w-[350px] h-[40px] bg-black
                         border-[1px] border-solid
                         border-gray-500 rounded-[5px]
                         text-white  mb-[60px]
                         "
-                    >
-                        Login
-                    </button>
-                </div>
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+
             </div>
         </div>
     )
